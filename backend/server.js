5ffd6b97ae419671a1484b00f1dba898d615ef5f
Adam Winek <adamwinek@gmail.com>
2020-10-12 22:44:24 -0600

    config changes numba 3

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/build')))
+app.use(express.static(path.join(__dirname, '../build')))
 
 
 
@@ -17,7 +17,7 @@ app.get('/api', (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+    res.sendFile(path.join(__dirname + '../build/index.html'));
 });
 
 app.listen(port, () => {
