924cc17269a7eb5811b644fa3056ee1461e18446
Adam Winek <adamwinek@gmail.com>
2020-11-21 22:53:40 -0500

    added notification api do not know the best way to do email

---
+++ backend/models/Subscription.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const userSchema = require("./User");
+
+const SubscriptionArray = new mongoose.Schema({
+    gameId: {
+        type: String,
+    },
+})
+
+const SubscriptionSchema = new mongoose.Schema({
+    type: {
+        type: String,
+    },
+
+    fname: {
+        type: String
+    },
+    lname: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    phone: {
+        type: String
+    },
+
+    // league name or team name or gameId
+    identifier: {
+        type: String
+    },
+    notifiedGames: {
+        type: [SubscriptionArray]
+    },
+    viaEmail: {
+        type: Boolean
+    },
+    viaText: {
+        type: Boolean
+    },
+    scoreCriteria: {
+        type: Number
+    },
+    timeCriteria: {
+        type: Number
+    },
+    onStart: {
+        type: Boolean
+    },
+    onEnd: {
+        type: Boolean
+    }
+
+})
+
+
+const Subscription = mongoose.model("Subscriptions", SubscriptionSchema);
+module.exports = Subscription
