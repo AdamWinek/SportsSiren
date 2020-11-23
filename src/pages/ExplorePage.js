0906fe1d08b0810074234e3528fd2381fd345d3e
Adam Winek <adamwinek@gmail.com>
2020-11-23 02:07:24 -0500

    added search component and new sub form

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import HomeNav from "../components/HomeNav";
 import RowCon from "../components/containers/RowCon";
 import styles from "../css/explore_page.module.css";
+import SearchBar from "../components/SearchBar";
 
 const ExplorePage = (props) => {
   return (
     <>
       <HomeNav active={"Explore"} />
       <div className={styles.body}>
+        <SearchBar />
+
         <h1 className={styles.title}>Explore </h1>
         <RowCon type="league"></RowCon>
         <RowCon type="team"></RowCon>
