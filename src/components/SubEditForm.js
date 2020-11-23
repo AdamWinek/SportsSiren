4b44cbbac250583ca3f81869e4cd08d176fd186e
Adam Winek <adamwinek@gmail.com>
2020-11-22 23:01:12 -0500

    fucked the merge

--- src/components/SubEditForm.js
+++ src/components/SubEditForm.js
@@ -94,148 +94,138 @@ function SubEditForm(props) {
 
         }
     });
-    console.log("here");
-    setData({
-        sms: smsBool,
-        email: emailBool,
-        startofgame: startofgameBool,
-        endofgame: endofgameBool,
-        within: withinBool,
-        time: minutes,
-        threshold: score,
-    });
-}
-  });
 
-let sliders = null;
-if (data.within) {
-    sliders = (
-        <>
-            <br></br>
-            <label className={styles.rangelabel}>
-                <h2 className={styles.questionTitle}>
-                    Game is ending in {data.time} minutes
+
+
+    let sliders = null;
+    if (data.within) {
+        sliders = (
+            <>
+                <br></br>
+                <label className={styles.rangelabel}>
+                    <h2 className={styles.questionTitle}>
+                        Game is ending in {data.time} minutes
           </h2>
+                </label>
+                <br></br>
+                <div className={styles.slideContainer}>
+                    <input
+                        className={styles.slider}
+                        type="range"
+                        min="1"
+                        max="59"
+                        name="time"
+                        onChange={(e) => onSlide(e)}
+                    ></input>
+                    <span className={styles.indicator}>{data.time} minutes</span>
+                </div>
+                <br></br>
+                <label className={styles.rangelabel}>
+                    <h2 className={styles.questionTitle}>
+                        Game is within {data.threshold} points
+          </h2>
+                </label>
+                <br></br>
+                <div className={styles.slideContainer}>
+                    <input
+                        className={styles.slider}
+                        type="range"
+                        min="0"
+                        max="21"
+                        name="threshold"
+                        onChange={(e) => onSlide(e)}
+                    ></input>
+                    <span className={styles.indicator}>{data.threshold} points</span>
+                </div>{" "}
+            </>
+        );
+    }
+
+    return (
+        <div>
+            <div className={styles.bodyContent}>
+                <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
+                    <div>
+                        <h2 className={styles.questionTitle}>Notify me via:</h2>
+                        <input
+                            type="checkbox"
+                            id="email"
+                            name="email"
+                            checked={data.email}
+                            className={styles.checkbox}
+                            onClick={(e) => onChange(e)}
+                        ></input>
+                        <label htmlFor="email" className={styles.label}>
+                            Email
             </label>
-            <br></br>
-            <div className={styles.slideContainer}>
-                <input
-                    className={styles.slider}
-                    type="range"
-                    min="1"
-                    max="59"
-                    name="time"
-                    onChange={(e) => onSlide(e)}
-                ></input>
-                <span className={styles.indicator}>{data.time} minutes</span>
+                        <br></br>
+                        <input
+                            type="checkbox"
+                            id="sms"
+                            name="sms"
+                            checked={data.sms}
+                            className={styles.checkbox}
+                            onClick={(e) => onChange(e)}
+                        ></input>
+                        <label htmlFor="sms" className={styles.label}>
+                            Text
+            </label>
+                    </div>
+                    <div>
+                        <h2 className={styles.questionTitle}>Notify me when:</h2>
+                        <input
+                            type="checkbox"
+                            id="startofgame"
+                            name="startofgame"
+                            value="Apple"
+                            checked={data.startofgame}
+                            className={styles.checkbox}
+                            onClick={(e) => onChange(e)}
+                        ></input>
+                        <label htmlFor="startofgame" className={styles.label}>
+                            Game starts
+            </label>
+                        <br></br>
+                        <input
+                            type="checkbox"
+                            id="endofgame"
+                            name="endofgame"
+                            value="Apple"
+                            checked={data.endofgame}
+                            className={styles.checkbox}
+                            onClick={(e) => onChange(e)}
+                        ></input>
+                        <label htmlFor="endofgame" className={styles.label}>
+                            Game ends
+            </label>
+                        <br></br>
+                        <input
+                            type="checkbox"
+                            id="within"
+                            name="within"
+                            checked={data.within}
+                            className={styles.checkbox}
+                            onClick={(e) => onChange(e)}
+                        ></input>
+                        <label htmlFor="score/time" className={styles.label}>
+                            Score and Time Threshold
+            </label>
+                        {sliders}
+
+                        <br></br>
+                        <button className={styles.btn} onClick={() => handleEditSubmit()}>
+                            Submit
+            </button>
+                        <button
+                            className={styles.cancelBtn}
+                            onClick={() => props.handleToggle()}
+                        >
+                            Cancel
+            </button>
+                    </div>
+                </form>
             </div>
-            <br></br>
-            <label className={styles.rangelabel}>
-                <h2 className={styles.questionTitle}>
-                    Game is within {data.threshold} points
-          </h2>
-            </label>
-            <br></br>
-            <div className={styles.slideContainer}>
-                <input
-                    className={styles.slider}
-                    type="range"
-                    min="0"
-                    max="21"
-                    name="threshold"
-                    onChange={(e) => onSlide(e)}
-                ></input>
-                <span className={styles.indicator}>{data.threshold} points</span>
-            </div>{" "}
-        </>
-    );
-}
-
-return (
-    <div>
-        <div className={styles.bodyContent}>
-            <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
-                <div>
-                    <h2 className={styles.questionTitle}>Notify me via:</h2>
-                    <input
-                        type="checkbox"
-                        id="email"
-                        name="email"
-                        checked={data.email}
-                        className={styles.checkbox}
-                        onClick={(e) => onChange(e)}
-                    ></input>
-                    <label htmlFor="email" className={styles.label}>
-                        Email
-            </label>
-                    <br></br>
-                    <input
-                        type="checkbox"
-                        id="sms"
-                        name="sms"
-                        checked={data.sms}
-                        className={styles.checkbox}
-                        onClick={(e) => onChange(e)}
-                    ></input>
-                    <label htmlFor="sms" className={styles.label}>
-                        Text
-            </label>
-                </div>
-                <div>
-                    <h2 className={styles.questionTitle}>Notify me when:</h2>
-                    <input
-                        type="checkbox"
-                        id="startofgame"
-                        name="startofgame"
-                        value="Apple"
-                        checked={data.startofgame}
-                        className={styles.checkbox}
-                        onClick={(e) => onChange(e)}
-                    ></input>
-                    <label htmlFor="startofgame" className={styles.label}>
-                        Game starts
-            </label>
-                    <br></br>
-                    <input
-                        type="checkbox"
-                        id="endofgame"
-                        name="endofgame"
-                        value="Apple"
-                        checked={data.endofgame}
-                        className={styles.checkbox}
-                        onClick={(e) => onChange(e)}
-                    ></input>
-                    <label htmlFor="endofgame" className={styles.label}>
-                        Game ends
-            </label>
-                    <br></br>
-                    <input
-                        type="checkbox"
-                        id="within"
-                        name="within"
-                        checked={data.within}
-                        className={styles.checkbox}
-                        onClick={(e) => onChange(e)}
-                    ></input>
-                    <label htmlFor="score/time" className={styles.label}>
-                        Score and Time Threshold
-            </label>
-                    {sliders}
-
-                    <br></br>
-                    <button className={styles.btn} onClick={() => handleEditSubmit()}>
-                        Submit
-            </button>
-                    <button
-                        className={styles.cancelBtn}
-                        onClick={() => props.handleToggle()}
-                    >
-                        Cancel
-            </button>
-                </div>
-            </form>
         </div>
-    </div>
-);
+    );
 }
 export default SubEditForm;
