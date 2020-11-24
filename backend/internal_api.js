434067f8ca42b56d57b8cebe2d10bf176d023fda
Adam Winek <adamwinek@gmail.com>
2020-11-23 22:22:01 -0500

    fixexd endpoint method bug

--- backend/internal_api.js
+++ backend/internal_api.js
@@ -296,11 +296,6 @@ async function handleNotification(req, res) {
     }
   }
 
-  let methodUrl = "https://sports-siren.herokuapp.com/api/"
-  console.log(process.env.REACT_APP_DEV_ENV)
-  if (process.env.REACT_APP_DEV_ENV == "development") {
-    methodUrl = "http://localhost:3000/api/"
-  }
 
 
   // time to add for each quarter
